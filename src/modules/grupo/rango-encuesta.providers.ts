6a0c338e971d40c6423cdc5bcb1eb798e14fec8c
joelpena270288 <joelpenagonzalez122@gmail.com>
2024-07-23 12:19:22 -0400

    cambio nombre

--- src/modules/grupo/rango-encuesta.providers.ts
+++ src/modules/grupo/rango-encuesta.providers.ts
@@ -2,7 +2,7 @@ import { DataSource } from 'typeorm';
 import {RangoEncuesta } from './entities/rango-encuesta.entity';
 export const RangoEncuestaProviders = [
   {
-    provide: 'RANGODENCUESTA_REPOSITORY',
+    provide: 'RANGOENCUESTA_REPOSITORY',
     useFactory: (dataSource: DataSource) => dataSource.getRepository(RangoEncuesta),
     inject: ['DATA_SOURCE'],
   },
